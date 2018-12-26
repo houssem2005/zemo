2363d625b0821b95e30e31598537b0fc5bfeb2e7
houssem2005 <46058993+houssem2005@users.noreply.github.com>
2018-12-26 17:31:29 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("525809211071004674")
+setInterval(function() {
+channel.send(`Zemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The bestZemoo The best`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
